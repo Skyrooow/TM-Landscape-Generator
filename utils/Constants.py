3740296f0cd5d762b98a9843b9c311d60e88d7b8
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-02 01:29:21 +0100

    Add BasePanel class

---
+++ utils/Constants.py
@@ -0,0 +1,9 @@
+"""All constants accross the addon"""
+
+
+
+class BasePanel():
+    """Data common to all panels"""
+    bl_category ="TM Scenery Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
