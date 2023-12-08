99f3c03c024e4fbbf92eb632d93fd4271f3e3572
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-08 22:54:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+# Import builtin modules
+import bpy
+import os
+
+
+
+# Addon directory name
+ADDON_DIRNAME = os.path.dirname(__file__)
+"""Main __init__.py directory"""
+
+
+
 # Blender addon info
 bl_info = {
     "name": "TM Scenery Tools",
@@ -12,26 +24,23 @@ bl_info = {
     "category": "3D View",
 }
 
-# Import builtin modules
-import bpy
-import os
-
-
-# Addon directory name
-ADDON_DIRNAME = os.path.dirname(__file__)
-"""Main __init__.py directory"""
 
 
 # import my modules
 from .utils import Handlers
+from .utils import Log
+
+# Logger declaration
+log = Log.get_master_logger(__name__, debug=True)
+
+
 
 # import my classes
 from .operators.OT_HelloWorld       import C_OT_HelloWorld
+from .operators.OT_Test             import C_OT_Test
 
 from .panels.PT_HelloWorld          import C_PT_HelloWorld
-from .panels.PT_WhereAmII           import C_PT_WhereAmI
-from .panels.PT_zzz                 import C_PT_zzz
-
+from .panels.PT_Test                import C_PT_Test
 
 # classes register order
 classes = (
@@ -39,12 +48,13 @@ classes = (
 
     # Operators
     C_OT_HelloWorld,
+    C_OT_Test,
 
     # Panels
     C_PT_HelloWorld,
-    C_PT_WhereAmI,
-    C_PT_zzz,
+    C_PT_Test,
 )
+
 
 
 # regiser addon
@@ -54,6 +64,9 @@ def register():
 
     Handlers.load_handlers()
 
+    log.info(f"{__name__} registered")
+    
+
 
 # unregister addon
 def unregister():
@@ -61,3 +74,5 @@ def unregister():
         bpy.utils.unregister_class(cls)
 
     Handlers.delete_handlers()
+
+    log.info(f"{__name__} unregistered")    
