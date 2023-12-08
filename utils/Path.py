e5a94a72af93ce5970f403f937b6491b9ab814b6
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-08 22:53:33 +0100

    Update Path.py

--- utils/Path.py
+++ utils/Path.py
@@ -16,9 +16,28 @@ def is_path_existing(pathname: str) -> bool:
     return os.path.exists(pathname)
 
 
+def join(path:str, *paths:str) -> str:
+    return os.path.join(path, *paths)
+
+
 def dirname(pathname: str) -> str:
     """Returns the directory component of a pathname."""
     return os.path.dirname(pathname)
+
+
+def get_addon_dirname() -> str:
+    """Returns the addon directory."""
+    return ADDON_DIRNAME
+
+
+def get_assets_dirname() -> str:
+    """Returns the addon assets directory."""
+    return join(get_addon_dirname(), 'assets')
+
+
+def get_logfile_path() -> str:
+    """Return the log file path"""
+    return join(get_addon_dirname(), 'log.txt')
 
 
 def get_blenderfile_path() -> str:
@@ -29,19 +48,3 @@ def get_blenderfile_path() -> str:
 def get_blenderfile_dirname() -> str:
     """Returns current blender file directory. Empty string if file not exists."""
     return dirname(get_blenderfile_path())
-
-
-def get_addon_dirname() -> str:
-    """Returns the addon directory."""
-    return ADDON_DIRNAME
-
-
-def get_assets_dirname() -> str:
-    """Returns the addon assets directory."""
-    return os.path.join(ADDON_DIRNAME, 'assets')
-
-
-def get_temp_dirname() -> str:
-    """Return current blender instance temporary directory."""
-    return native_path(bpy.app.tempdir[:-1])
-
