32c8094b796d690cb008faa6881e5113c58fb082
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-11 23:58:35 +0100

    Create __init__.py

---
+++ panels/__init__.py
@@ -0,0 +1,8 @@
+"""Initialze panels package"""
+
+
+class Panels():
+    """Data common to all panels"""
+    bl_category ="TM Scenery Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
