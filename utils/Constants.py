793f406986751c06074a86057a18886da336ef0d
Skyrooow <151015957+Skyrooow@users.noreply.github.com>
2023-12-11 23:59:40 +0100

    Update Constants.py

--- utils/Constants.py
+++ utils/Constants.py
@@ -1,7 +1,1 @@
 """All constants accross the addon"""
-
-class BasePanel():
-    """Data common to all panels"""
-    bl_category ="TM Scenery Tools"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
